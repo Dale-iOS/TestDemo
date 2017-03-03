36908e17a76a193fce646d1d0cc970abf6c19f7e
cjdale <cjdale@163.com>
2017-03-03 14:17:32 +0800

    Initial Commit

---
+++ TestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDemo
+//
+//  Created by Dale on 17/3/3.
+//  Copyright © 2017年 Nari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestDemo
+//
+//  Created by Dale on 17/3/3.
+//  Copyright © 2017年 Nari. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
